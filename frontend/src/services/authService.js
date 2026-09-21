ac6af1c4b770294fb81dae557e02ec42e2692478
Arquam Ali Shakeeb <arquamalishakeeb@gmail.com>
2026-09-21 14:34:26 +0500

    vercel frontend fix

--- frontend/src/services/authService.js
+++ frontend/src/services/authService.js
@@ -1,6 +1,6 @@
 import { apiClient } from "./apiClient";
 
-const API_URL = import.meta.env.VITE_API_URL/auth;
+const API_URL = `${import.meta.env.VITE_API_URL}/auth`;
 
 // =========================
 // Helper
