b54522d2ec99178d2d4e47e02838eede230556e5
Arquam Ali Shakeeb <arquamalishakeeb@gmail.com>
2026-09-21 13:50:56 +0500

    fix pdf canvas dependency

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,27 @@
 import "dotenv/config";
 
+import dns from "dns";
 import app from "./app.js";
 import connectDB from "./config/db.js";
 
+
+// =========================
+// DNS
+// =========================
+
+dns.setServers([
+    "8.8.8.8",
+    "8.8.4.4"
+]);
+
+
+// =========================
+// Server
+// =========================
+
 const PORT =
     process.env.PORT || 5000;
+
 
 const startServer = async () => {
 
@@ -19,5 +36,6 @@ const startServer = async () => {
     });
 
 };
+
 
 startServer();
